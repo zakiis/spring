e4249eba0ef0af739be1b04135d94840549bb80e
刘政华 <liuzhenghua@liuzhenghuadeMacBook-Air.local>
2022-03-25 18:41:42 +0800

    zakiis-spring

--- src/main/java/com/zakiis/boot/autoconfigure/MybatisCipherAutoConfiguration.java
+++ src/main/java/com/zakiis/boot/autoconfigure/MybatisCipherAutoConfiguration.java
@@ -6,5 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableConfigurationProperties(MybatisCipherProperties.class)
 public class MybatisCipherAutoConfiguration {
+	
+	
 
 }
